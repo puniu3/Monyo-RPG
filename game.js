d3006f8a2d3e3860f01c8ef14c4e179adbd0a5c4
Hiroshi Nishimura <puniu3@gmail.com>
2022-01-05 11:09:43 +0900

    make it endless

--- game.js
+++ game.js
@@ -1,59 +1,71 @@
 export default function*(){
-    let pl = {
-        maxHp: 10,
-        hp: 10,
-        blows: 2,
-        attack: [1, 4],
-        effects:[{type: "dodge", amount: 0.1, duration: Infinity}],
-    };
-    
-    let kills = 0;
+    let xp = 0;
     
     while(true){
-        let enemy = {...orc, effects:[]};
-        yield [enemy.name + "がおそってきた！", "たたかう"];
+        let pl = {
+            maxHp: 10,
+            hp: 10,
+            blows: 2,
+            attack: [1, 4],
+            effects:[{type: "dodge", amount: 0.1, duration: Infinity}],
+            skills:["なぐる", "かいふく", "いなずま", "ぼうぎょ"]
+        };
+        
+        let kills = 0;
         
         while(true){
+            let enemy = {...orc, effects:[]};
+            yield [enemy.name + "がおそってきた！", "たたかう"];
+            
+            while(true){
+                pl.effects = pl.effects.map(e => ({...e, duration: e.duration - 1})).filter(({duration}) => duration > 0);
+                
+                let cmd = yield [`たいりょく：${pl.hp}/${maxHp(pl)}\nじゅついりょく：${multiplier(pl.effects, "spell") * 100}%\nてき：${enemy.name}`, ...pl.skills];
+                const act = pl.skills[cmd - 1];
+                if(act === "なぐる"){
+                    const dmg = attack(...pl.attack, pl.blows);
+                    enemy.hp -= dmg;
+                    yield [`ぽかっすかっ！${dmg}のダメージ！${enemy.hp > 0 ? "" : enemy.name + "はうごかなくなった！"}`, "つづける"];
+                }else if(act === "かいふく"){
+                    const rst = round(rnd(3, 8) * multiplier(pl.effects, "spell"));
+                    pl.hp = Math.min(maxHp(pl), pl.hp + rst);
+                    pl.effects.push({type: "spell", amount: -0.25, duration: 3});
+                    pl.effects.push({type: "hp", amount: -0.04, duration: Infinity});
+                    yield [`いたいのとんでけ！${rst}のたいりょくをかいふく！`, "つづける"];
+                }else if(act === "いなずま"){
+                    const dmg = round(6 * multiplier(pl.effects, "spell"));
+                    enemy.hp -= dmg;
+                    pl.effects.push({type: "spell", amount: -0.25, duration: 3});
+                    yield [`ニンポをつかうぞ！${dmg}のダメージ！${enemy.hp > 0 ? "" : enemy.name + "はうごかなくなった！"}`, "つづける"];
+                }else if(act === "ぼうぎょ"){
+                    pl.effects.push({type: "dodge", amount: 0.5, duration: 2});
+                    yield ["みがまえて こうげきをかわしやすくなった！", "つづける"];
+                }
+                if(enemy.hp <= 0) break;
+                
+                const dmg = pickRnd(enemy.damage);
+                if(Math.random() > sum(pl.effects, "dodge", 0.85)){
+                    pl.hp -= dmg;
+                    yield [`がすっ！${dmg}のダメージをうけた！${pl.hp > 0 ? "" : "やられてしまった！"}`, "つづける"];
+                }else{
+                    yield [`ひらり！こうげきをかわした！`, "つづける"];
+                }
+                if(pl.hp <= 0) break;
+            }
+            
+            if(pl.hp <= 0) return [`しんでしまった.. たおしたてき：${kills}`];
+            
+            ++kills;        
+            const cmd = yield ["たたかいはおわった！", "すすむ", "もどる"];
+            if(cmd === 2) break;
+            
+            pl.hp = Math.min(maxHp(pl), pl.hp + round(maxHp(pl) * 0.25));
             pl.effects = pl.effects.map(e => ({...e, duration: e.duration - 1})).filter(({duration}) => duration > 0);
-            
-            let cmd = yield [`たいりょく：${pl.hp}/${maxHp(pl)}\nじゅついりょく：${multiplier(pl.effects, "spell") * 100}%\nてき：${enemy.name}`, "なぐる", "かいふく", "いなずま", "ぼうぎょ"];
-            if(cmd === 1){
-                const dmg = attack(...pl.attack, pl.blows);
-                enemy.hp -= dmg;
-                yield [`ぽかっすかっ！${dmg}のダメージ！${enemy.hp > 0 ? "" : enemy.name + "はうごかなくなった！"}`, "つづける"];
-            }else if(cmd === 2){
-                const rst = round(rnd(3, 8) * multiplier(pl.effects, "spell"));
-                pl.hp = Math.min(maxHp(pl), pl.hp + rst);
-                pl.effects.push({type: "spell", amount: -0.25, duration: 3});
-                pl.effects.push({type: "hp", amount: -0.04, duration: Infinity});
-                yield [`いたいのとんでけ！${rst}のたいりょくをかいふく！`, "つづける"];
-            }else if(cmd === 3){
-                const dmg = round(5 * multiplier(pl.effects, "spell"));
-                enemy.hp -= dmg;
-                pl.effects.push({type: "spell", amount: -0.25, duration: 3});
-                yield [`ニンポをつかうぞ！${dmg}のダメージ！${enemy.hp > 0 ? "" : enemy.name + "はうごかなくなった！"}`, "つづける"];
-            }else if(cmd === 4){
-                pl.effects.push({type: "dodge", amount: 0.5, duration: 2});
-                yield ["みがまえて こうげきをかわしやすくなった！", "つづける"];
-            }
-            if(enemy.hp <= 0) break;
-            
-            const dmg = pickRnd(enemy.damage);
-            if(Math.random() > sum(pl.effects, "dodge", 0.85)){
-                pl.hp -= dmg;
-                yield [`がすっ！${dmg}のダメージをうけた！${pl.hp > 0 ? "" : "やられてしまった！"}`, "つづける"];
-            }else{
-                yield [`ひらり！こうげきをかわした！`, "つづける"];
-            }
-            if(pl.hp <= 0) break;
         }
         
-        if(pl.hp <= 0) return [`しんでしまった.. たおしたてき：${kills}`];
-        
-        yield ["たたかいはおわった！", "さらにたたかう"];
-        ++kills;
-        pl.hp = Math.min(maxHp(pl), pl.hp + round(maxHp(pl) * 0.25));
-        pl.effects = pl.effects.map(e => ({...e, duration: e.duration - 1})).filter(({duration}) => duration > 0);
+        const xpGained = xpTable[kills] || xpTable[xpTable.length - 1];
+        yield [`ぶじにかえってきた..\n${kills}たいのてきをたおし ${xpGained}のけいけんをえた${level(xp) < level(xp + xpGained) ? "\nレベルがあがった！" : ""}`, "つづける"];
+        xp += xpGained;
     }
 }
 
@@ -78,3 +90,7 @@ const orc = {
     hp: 12,
     damage: [6, 3, 2, 2]
 };
+
+const xpTable = [0, 1, 2, 4, 6, 10, 12, 14, 15];
+const lvTable = [0, 3, 10, 20, 38];
+const level = xp => lvTable.filter(req => req <= xp).length;
